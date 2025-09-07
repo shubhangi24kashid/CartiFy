a95c16581494928e2e0b28b9dc52f04aa1053c86
shubhangi24kashid <kashidshubhangi24@gmail.com>
2025-09-07 20:41:41 +0530

    Dynamic port added

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ app.use(cors());
 // http://localhost:5000/api/products
 app.use('/api', productRouter);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
